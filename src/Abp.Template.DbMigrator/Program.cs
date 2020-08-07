05479103848b07cf5834725bc81b98411f73b056
阿星Plus <hackxing@foxmail.com>
2020-08-07 17:21:52 +0800

    Abp.Template.DbMigrator

---
+++ src/Abp.Template.DbMigrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abp.Template.DbMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
